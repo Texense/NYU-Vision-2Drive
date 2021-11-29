ccf294a9b45489cbcc241775f8aafae7a0bcb5e4
Texense <32527355+Texense@users.noreply.github.com>
2021-11-29 11:50:02 -0500

    home

--- Utils/PoissonInputForNetwork.m
+++ Utils/PoissonInputForNetwork.m
@@ -2,12 +2,13 @@
 % Input: N: Number of Neurons
 %        lambda: Rates of Poisson; Can be N*1 vec or 1 number
 %        T: Total time (Unit: ms)
+%        Normal: boolean: normalization or not
 
 % Output: eventsN: N*X Mat containing the series time for each neuron.
 %         EventTimeMat: N*timebins, entries are number of events of
 %         corresponding neuron in bin
 % single by default
-function EventTimeMat = PoissonInputForNetwork(N,lambda,T,dt)
+function EventTimeMat = PoissonInputForNetwork(N,lambda,T,dt, varargin)
 switch length(lambda)
     case 1
         lambda_use = lambda*ones(N,1);
@@ -29,6 +30,14 @@ end
 [A,~,Times] = find(eventsN);
 EventTimeMat = full(sparse(A,floor(Times/dt)+1,ones(size(A)),N,floor(T/dt)+1));
 
+if ~isempty(varargin)
+    normal = varargin{1};
+    if normal
+        norm4sum = sum(EventTimeMat,2)/T./lambda;
+        EventTimeMat = EventTimeMat./repmat(norm4sum,1,size(EventTimeMat,2));
+    end
+    
+end
 
 end
 
