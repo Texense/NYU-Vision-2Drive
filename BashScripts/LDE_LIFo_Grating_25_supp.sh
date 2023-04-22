74eba96577aba44161f3b25f39f2d08187697046
Texense <32527355+Texense@users.noreply.github.com>
2023-04-21 23:25:56 -0400

    The first validation of monocular inputs

--- BashScripts/LDE_LIFo_Grating_25_supp.sh
+++ BashScripts/LDE_LIFo_Grating_25_supp.sh
@@ -2,12 +2,12 @@ declare -a AngleMat LGNctgrMat L6ctgrMat
 ##AngleMat=(0 0 15 15 15 15 15 15 22.5 22.5 7.5 7.5 7.5)
 ##LGNctgrMat=(4 4 1 1 2 2 2 4 1 2 1 2 3)
 ##L6ctgrMat=(1 2 2 3 1 2 4 4 1 2 3 2 4)
-AngleMat=(0 0 0)
-LGNctgrMat=(1 5 5)
-L6ctgrMat=(5 1 1)
-FlagLargeMat=(1 2 3)
+AngleMat=(0 0 0 0)
+LGNctgrMat=(1 1 3 3)
+L6ctgrMat=(3 4 4 4)
+FlagLargeMat=(3 1 1 2)
 #
-for MatInd in $(seq 0 2); do
+for MatInd in $(seq 0 3); do
 for LGNL6Mapctgr in 3; do
 #
 Angle=${AngleMat[${MatInd}]}
