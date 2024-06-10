4e6a825cac7dc8ab04099d39104ab1c9fe062378
Texense <32527355+Texense@users.noreply.github.com>
2024-06-10 17:00:30 -0400

    Proceeding to HPC local and global tests

--- Utils/InhMulp.m
+++ Utils/InhMulp.m
@@ -22,7 +22,13 @@ elseif strcmp(IKp1.Mode,'sigmoid') == 1
     % rescale the outcome of sigm
     sigmRescle = (sigm - low)/(high-low);
     Factor(InterpId) = 1 + sigmRescle * (IKp1.Slope - 1);
-elseif strcmp(IKp1.Mode,'mutisigmoid') == 1 % multiple lags of sigmoid
+elseif strcmp(IKp1.Mode,'multisigmoid') == 1 % multiple lags of sigmoid
+    if isfield(IKp1,'down1')
+        down1 = IKp1.down1; % how much does the first part goes down? should be between 0 and 1
+    else
+        down1 = 0.5; % 0.5 by default
+    end
+    down2 = 1-down1;
     InterpId2 = LDEUse<IKp1.Highist & LDEUse>=IKp1.Thrsld2;
     InterpId1 = LDEUse<=IKp1.Thrsld2 & LDEUse>=IKp1.Thrsld1;
     aL = IKp1.IntaL; bL = IKp1.IntbL;
@@ -33,7 +39,7 @@ elseif strcmp(IKp1.Mode,'mutisigmoid') == 1 % multiple lags of sigmoid
     highbb = sigmod(bH); lowbb = sigmod(-bL);
     % rescale the outcome of sigm
     sigmResclebb = (sigmbb - lowbb)/(highbb-lowbb);
-    sigmResclebb = sigmResclebb/max(sigmResclebb,[],'all') * 0.5;% rescale to make sure that the end goes down one half
+    sigmResclebb = sigmResclebb/max(sigmResclebb,[],'all') * down1;% rescale to make sure that the end goes down one half
     Factor(InterpId1) = 1 + sigmResclebb * (IKp1.Slope - 1);
     % for lag 2
     xxaa = (LDEUse(InterpId2) - IKp1.Thrsld2)/(IKp1.Highist - IKp1.Thrsld2) * (aH+aL) -aL;
@@ -42,7 +48,7 @@ elseif strcmp(IKp1.Mode,'mutisigmoid') == 1 % multiple lags of sigmoid
     % rescale the outcome of sigm
     sigmRescleaa = (sigmaa - lowaa)/(highaa-lowaa);
     sigmRescleaa = (sigmRescleaa - min(sigmRescleaa,[],'all')) / ...
-        max(sigmRescleaa - min(sigmRescleaa,[],'all'),[],'al') *0.5 + 0.5;
+        max(sigmRescleaa - min(sigmRescleaa,[],'all'),[],'al') *down2 + down1;
     Factor(InterpId2) = 1 + sigmRescleaa * (IKp1.Slope - 1);
 
 end
--- Utils/L6Convert.m
+++ Utils/L6Convert.m
@@ -2,31 +2,48 @@
 % L6pars is a cell containing parameters for the linear function
 
 function y = L6Convert(x, L6pars)
-    % Apply the initial linear mapping
     y = L6pars{1} * (x - L6pars{2});
+    % Apply the initial linear mapping
+    if strcmp(L6pars{end},'quadratic') 
+        % in this case, we will use the following three points to determine
+        % an inverse function of quadratic function
+        x_data = L6pars{4}; y_data = L6pars{3};
+        % Create the matrix A for the system of equations
+        A = [x_data.^2, x_data, ones(3, 1)];
 
-    % Number of pairs beyond the threshold
-    numPairs = (length(L6pars) - 3) / 2;
-    if length(L6pars)>=3
-        % Arrays for piecewise linear interpolation
-        xPoints = [L6pars{3}];  % Starting with the threshold
-        yPoints = [L6pars{3}];  % Assuming y = x at the threshold
+        % Solve for the coefficients [a; b; c]
+        Quad_coeff = A \ y_data;
 
-        % Construct the arrays for interpolation based on the provided rules
-        for n = 2:numPairs+1
-            yPoints(end+1) = L6pars{2*n+1};  % y values to be mapped
-            xPoints(end+1) = L6pars{2*n};    % Corresponding y values after mapping
+        % Display the coefficients
+        a = Quad_coeff(1);
+        b = Quad_coeff(2);
+        c = Quad_coeff(3);
+        % fprintf('The quadratic function is y = %.4fx^2 + %.4fx + %.4f\n', a, b, c);
+        xAdj = x(x > y_data(1));
+        y(y > x_data(1)) = (-b + sqrt(b^2 - 4*a*c + 4*a*xAdj)) / (2*a);
+    else % piecewise linear
+        % Number of pairs beyond the threshold
+        numPairs = (length(L6pars) - 3) / 2;
+        if length(L6pars)>=3
+            % Arrays for piecewise linear interpolation
+            xPoints = [L6pars{3}];  % Starting with the threshold
+            yPoints = [L6pars{3}];  % Assuming y = x at the threshold
+
+            % Construct the arrays for interpolation based on the provided rules
+            for n = 2:numPairs+1
+                yPoints(end+1) = L6pars{2*n+1};  % y values to be mapped
+                xPoints(end+1) = L6pars{2*n};    % Corresponding y values after mapping
+            end
+
+            % Ensure xPoints are in increasing order along with their corresponding yPoints
+            [xPoints, sortIdx] = sort(xPoints, 'ascend');
+            yPoints = yPoints(sortIdx);
+            IdInter = y > L6pars{3} & y <= xPoints(end);
+            IdExter = y > xPoints(end);
+            y(IdInter) = interp1(xPoints, yPoints, y(IdInter), 'linear');
+            y(IdExter) = interp1(xPoints, yPoints, y(IdExter), 'linear', 'extrap');
         end
-
-        % Ensure xPoints are in increasing order along with their corresponding yPoints
-        [xPoints, sortIdx] = sort(xPoints, 'ascend');
-        yPoints = yPoints(sortIdx);
-        IdInter = y > L6pars{3} & y <= xPoints(end);
-        IdExter = y > xPoints(end);
-        y(IdInter) = interp1(xPoints, yPoints, y(IdInter), 'linear');
-        y(IdExter) = interp1(xPoints, yPoints, y(IdExter), 'linear', 'extrap');
     end
-
 end
 
 
